c57d29a551118fdfb43111a5cd8cf1730a5c777b
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-03-20 19:07:17 -0500

    initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../Todo.css';
+
+class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: props.description,
+            done: false
+        };
+    }
+
+    render() {
+        return <div className="Todo">{this.state.description}</div>;
+    }
+}
+
+export default Todo;
