959618251c52d0f1e5276476d49d1dcfc4531b75
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-03-20 20:07:50 -0500

    add mark as done button functionality and css properties

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -15,6 +15,14 @@ class Todo extends Component {
         this.setState({ done: true });
     }
 
+    cssClasses() {
+        let classes = ['Todo'];
+        if (this.state.done) {
+            classes = [...classes, 'Done'];
+        }
+        return classes.join(' ');
+    }
+
     render() {
         return (
             <div className={"Todo" + (this.state.done ? ' Done' : '')}>
